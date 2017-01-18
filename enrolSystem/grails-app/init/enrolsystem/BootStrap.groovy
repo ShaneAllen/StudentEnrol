7e6c9e44e189753e1f14068696234cd83e698c61
ShaneAllen <b5003704@my.shu.ac.uk>
2017-01-18 11:37:31 +0000

    Lecturer and module added

--- enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
+++ enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -1,8 +1,24 @@
-package enrolsystem
+package com.enrol
 
 class BootStrap {
 
     def init = { servletContext ->
+	def module = new Module(
+	title: 'System Architecture',
+	code: '33333333',
+	credits: 20,
+	lecturer: 'Dr Shane',
+	description: '''Hello''',
+	course: 'System'
+	).save()
+	def lecturer = new Lecturer(
+	fullName: 'DR SHANE ALLEN',
+	post: 'HEAD LECTURER',
+	subject: 'COMPUTING',
+	lecturerEmail: 'SHANE.ALLEN@SHU.COM',
+	office: 'CANTOR2003',
+	bio: '''TOP'''
+	).save()
     }
     def destroy = {
     }
